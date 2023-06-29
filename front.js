7eac55aade2a792043ce6c652829cb99f7b9fc5b
Abhay <abhaykumar7007gupta@gmail.com>
2023-06-29 20:49:55 +0530

    initial commit

---
+++ front.js
@@ -0,0 +1,13 @@
+
+
+var i = 0;
+var txt = 'Welcome to this amazing website'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("intro").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
